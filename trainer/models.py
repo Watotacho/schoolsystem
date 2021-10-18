9133a073c8eaadedb4a2f4d9db08ec2846532c89
Watotacho <watotacho@gmail.com>
2021-10-18 22:46:10 +0300

    secod commit

--- trainer/models.py
+++ trainer/models.py
@@ -6,12 +6,12 @@ class trainer(models.Model):
     age=models.PositiveSmallIntegerField()
     date_of_birth=models.DateField()
     phone_number=models.CharField(max_length=12)
-    company_name=models.CharField(max_length=12)
+    resume=models.FileField()
     course_name=models.CharField(max_length=15)
     email=models.CharField(max_length=12)
-    resume=models.FileField()
     city=models.CharField(max_length=10)
     salary=models.BigIntegerField()
+    company_name=models.CharField(max_length=12)
     image=models.ImageField(upload_to="images/",null=True)
 
 
